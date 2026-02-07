1cc98f2219c57d0726b0490dd8a5e56ef1727bf2
Brittany Pollock <brittpollock15@gmail.com>
2026-02-07 13:08:57 -0800

    Initial Commit

---
+++ FunMultiplicationTables/FunMultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunMultiplicationTablesApp.swift
+//  FunMultiplicationTables
+//
+//  Created by Brittany Pollock on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct FunMultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
